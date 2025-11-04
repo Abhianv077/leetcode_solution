19ad99a1e61fb5d715ac0c2d118de8e2eb5832dd
Abhianv077 <168178160+Abhianv077@users.noreply.github.com>
2025-11-04 18:53:34 +0530

    Time: 11 ms (66.43%), Space: 46.9 MB (5.31%) - LeetHub

---
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int bestClosingTime(String customers) {
+        int n = customers.length();
+        int penalty = 0;
+
+       
+        for (int i = 0; i < n; i++) {
+            if (customers.charAt(i) == 'Y') {
+                penalty++;
+            }
+        }
+
+        int minPenalty = penalty;
+        int minHour = 0;
+
+        
+        for (int i = 0; i < n; i++) {
+            if (customers.charAt(i) == 'Y') {
+                penalty--;
+            } else {
+                penalty++;
+            }
+
+            if (penalty < minPenalty) {
+                minPenalty = penalty;
+                minHour = i + 1; 
+            }
+        }
+
+        return minHour;
+    }
+}
